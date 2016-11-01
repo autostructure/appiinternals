2b5bc94cb6e95ef7e49d7768ed4e3be6c4b52e01
Bryan Belanger <bbelanger@azcender.com>
2016-11-01 13:51:50 -0400

    Update with new package in Artifactory

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,9 @@
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class appinternals (
-  $analysis_server = $::appinternals::params::analysis_server,
-) inherits ::appinternals::params {
-  # validate parameters here
-  unless $analysis_server {
-    fail('Analysis server hostname by nodename, FQDN, or ip address is required')
-  }
-
+  String $analysis_server,
+  String $download_file_url,
+) {
   class { '::appinternals::install': } ->
   class { '::appinternals::config': } ~>
   class { '::appinternals::service': } ->
--- manifests/install.pp
+++ manifests/install.pp
@@ -4,12 +4,12 @@
 #
 class appinternals::install {
   # Appinsternals install script
-  $appinternals_install_stdin = @("EOT")
+  $appinternals_install_stdin = @('EOT')
     <<END
     1
     /tmp
-    opnet
-    /opt/opnet
+    S_RIVERBED
+    /opt/riverbed
     y
     $::appinternals::analysis_server
     y
@@ -17,32 +17,17 @@ class appinternals::install {
     END
     | EOT
 
-  # Create opnet user
-  user {'opnet':
-    ensure => present,
-    home   => '/home/opnet',
-    before => File['/home/opnet'],
-  }
-
-  # Create opnet user home directory
-  file {'/home/opnet':
-    ensure => directory,
-    before => ::Staging::Deploy['appinternals_agent_latest_linux.gz'],
-    owner  => 'opnet',
-  }
-
   # Download latest install and unpack to opnet home dir
   staging::deploy { 'appinternals_agent_latest_linux.gz':
-    source => 'http://download.appinternals.com/agents/a/appinternals_agent_latest_linux.gz',
-    target => '/home/opnet',
+    source => 'http://artifactory.azcender.com/artifactory/application-release-local/com/appinternals/appinternals_agent_linux/appinternals_agent_linux-v10.4.0.582.gz',
+    target => '/tmp',
     notify => [
-      File['/home/opnet/appinternals_agent_latest_linux'],
-      Exec["appinternals_agent_latest_linux ${appinternals_install_stdin}"]
+      File['/tmp/appinternals_agent_latest_linux']
     ],
   }
 
   # Make sure script is executable
-  file {'/home/opnet/appinternals_agent_latest_linux':
+  file {'/tmp/appinternals_agent_latest_linux':
     mode   => '0755',
     before =>
       Exec["appinternals_agent_latest_linux ${appinternals_install_stdin}"],
@@ -50,16 +35,16 @@ class appinternals::install {
 
   # Run the script onetime after unpack
   exec {"appinternals_agent_latest_linux ${appinternals_install_stdin}":
-    path        => '/home/opnet',
+    path        => '/tmp',
     refreshonly => true,
     notify      => Exec['dsactl start'],
   }
 
   # Run the script onetime after unpack
   exec {'dsactl start':
-    cwd         => '/home/opnet',
+    cwd         => '/opt/riverbed/Panorama/hedzup/mn/bin',
     path        => [
-      '/opt/opnet/Panorama/hedzup/mn/bin',
+      '/opt/riverbed/Panorama/hedzup/mn/bin',
       '/usr/local/sbin',
       '/sbin',
       '/bin',
@@ -69,7 +54,7 @@ class appinternals::install {
       '/usr/local/bin',
       '/opt/puppetlabs/bin',
     ],
-    user        => 'opnet',
+    user        => 'S_RIVERBED',
     refreshonly => true,
   }
 }
