7a1fac4ce4415ebcd1e0d0f6fb3538620b8d364a
Bryan Belanger <bbelanger@azcender.com>
2016-11-04 01:48:24 -0400

    huh ha

--- manifests/pre_install.pp
+++ manifests/pre_install.pp
@@ -9,11 +9,8 @@ class appinternals::pre_install {
   }
 
   # Download latest install and unpack to opnet home dir
-  # staging::deploy { 'appinternals_agent_latest_linux.gz':
-  #   source => $::appinternals::download_file_url,
-  #   target => '/tmp',
-  #   notify => [
-  #     File['/tmp/appinternals_agent_latest_linux']
-  #   ],
-  # }
+  staging::deploy { 'appinternals_agent_latest_linux.gz':
+    source => $::appinternals::download_file_url,
+    target => $::appinternals::extract_directory,
+  }
 }
