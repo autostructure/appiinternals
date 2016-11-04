ab6795793cb2f2d473d2857465b42e1e5fe88f68
Bryan Belanger <bbelanger@azcender.com>
2016-11-04 01:55:32 -0400

    qwfwq

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -16,8 +16,8 @@ describe 'appinternals' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
-        facts[:smell] = 'poo'
-        
+        facts[:staging_http_get] = 'curl'
+
         let(:facts) do
           facts
         end
