6d300b540002469118e69107ceaaaef4d700de2b
Bryan Belanger <bbelanger@azcender.com>
2016-11-09 12:47:01 -0500

    Add install properties file

--- manifests/pre_install.pp
+++ manifests/pre_install.pp
@@ -8,4 +8,20 @@ class appinternals::pre_install {
     source => $::appinternals::download_file_url,
     target => $::appinternals::extract_directory,
   }
+
+  file {'/tmp/install.sh':
+    ensure  => present,
+    content => epp(
+      'appinternals/storage.properties.epp',
+      {
+        user_account              => ::appinternals::user_account,
+        install_directory         => ::appinternals::install_directory,
+        extract_directory         => ::appinternals::extract_directory,
+        analysis_server_host      => ::appinternals::analysis_server_host,
+        analysis_server_port      => ::appinternals::analysis_server_port,
+        is_analysis_server_secure => ::appinternals::is_analysis_server_secure,
+        is_auto_instrument        => ::appinternals::is_auto_instrument,
+      }
+    )
+  }
 }
